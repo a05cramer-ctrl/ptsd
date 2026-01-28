2d6c6095dfa5f4a4d5044193d8bbb74eaa0bf603
PTSD Dev <dev@ptsd.sol>
2026-01-28 01:09:31 +0100

    Update buy and ca links in links.ts to new token URLs

--- src/lib/links.ts
+++ src/lib/links.ts
@@ -1,5 +1,5 @@
 export const LINKS = {
   twitter: 'https://x.com/PTSD_DEV',
-  buy: 'https://letsbonk.io/token/76Hm4ps4ag4x9jEarTXEDd5J7Vcziq1UMu2QGsK4bonk',
-  ca: '76Hm4ps4ag4x9jEarTXEDd5J7Vcziq1UMu2QGsK4bonk',
+  buy: 'https://letsbonk.io/token/DkAvHrs3ZnYnK96sWni76wmbg4pD3TRaJANxPfkxbonk',
+  ca: 'DkAvHrs3ZnYnK96sWni76wmbg4pD3TRaJANxPfkxbonk',
 } as const
